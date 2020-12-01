7c0af2167c801a52ce0bd8d0d8eda28eff217fa9
Kasun Hewawasam <kasun@Kasuns-MacBook-Pro.local>
2020-12-01 17:25:22 +0530

    first commit

---
+++ LiveStreamTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveStreamTest
+//
+//  Created by Kasun Hewawasam on 2020-12-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
